21830a760ebc52323da29eeb7d9d7c302f20e139
Steven Yao <stevenyao2016@gmail.com>
2024-08-03 23:04:10 -0700

    added search and more structure

--- app/page.js
+++ app/page.js
@@ -32,7 +32,10 @@ export default function Home() {
   const [inventory, setInventory] = useState([])
   const [open, setOpen] = useState(false)
   const [itemName, setItemName] = useState('')
-
+  const [searchQuery, setSearchQuery] = useState(""); 
+  const filteredSearch = inventory.filter((item) =>
+    item.name.toLowerCase().includes(searchQuery.toLowerCase())
+  );
   const updateInventory = async () => {
     const snapshot = query(collection(firestore, 'inventory'))
     const docs = await getDocs(snapshot)
@@ -76,88 +79,116 @@ export default function Home() {
   const handleOpen = () => setOpen(true)
   const handleClose = () => setOpen(false)
   return (
-    <Box
-      width="100vw"
-      height="100vh"
-      display={'flex'}
-      justifyContent={'center'}
-      flexDirection={'column'}
-      alignItems={'center'}
-      gap={2}
-    >
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
+    <Box>
+      <Box
+        width="100vw"
+        height="10%"
+        display={'flex'}
+        justifyContent={'center'}
+        flexDirection={'row'}
+        alignItems={'center'}
+        gap={2}
       >
-        <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Add Item
-          </Typography>
-          <Stack width="100%" direction={'row'} spacing={2}>
-            <TextField
-              id="outlined-basic"
-              label="Item"
-              variant="outlined"
-              fullWidth
-              value={itemName}
-              onChange={(e) => setItemName(e.target.value)}
-            />
-            <Button
-              variant="outlined"
-              onClick={() => {
-                addItem(itemName)
-                setItemName('')
-                handleClose()
-              }}
-            >
-              Add
-            </Button>
+        <Typography variant="h2">Pantry Tracker</Typography>
+      </Box>
+      <Box 
+        width={"100vw"}
+        height={"100vh" }
+        display={'flex'} 
+        justifyContent={'center'}
+        flexDirection={'column'} 
+        alignItems={'center'}
+        gap={2}
+      >
+        
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={style}>
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+              Add Item
+            </Typography>
+            <Stack width={'100%'} direction={'row'} spacing={2}>
+              <TextField 
+                id="outlined-basic" 
+                label="Item" 
+                variant="outlined" 
+                fullWidth
+                value={itemName}
+                onChange={(e) => setItemName(e.target.value)}
+              />
+              <Button variant="outlined" 
+                onClick={() => {
+                  addItem(itemName)
+                  setItemName('')
+                  handleClose()
+                }}
+              >
+                Add</Button>
+            </Stack>
+          </Box>
+        </Modal>
+        <Button variant="contained" onClick={handleOpen}>Add Item</Button>
+        <Box border={'1px solid #333'}>
+          <TextField
+            variant="outlined"
+            placeholder="Search items..."
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+            sx={{ width: "800px" }}
+          />
+          <Box 
+            width={'800px'} 
+            height={'100px'} 
+            bgcolor={'#ADDBE6'}
+            display={'flex'}
+            justifyContent={'center'}
+            alignItems={'center'}
+          >
+            <Typography variant={'h2'} color={'#333'} textAlign={'center'}>
+              Pantry Items
+            </Typography>
+          </Box>
+          <Stack width="800px" height="300px" spacing={2} overflow={'auto'}>
+            {filteredSearch.map(({name, quantity}) => (
+              <Box 
+                key={name}
+                width="100%" 
+                minHeight="100px" 
+                display={'flex'} 
+                justifyContent={'space-between'} 
+                alignItems={'center'}
+                bgcolor={'#f0f0f0'}
+                padding={3}
+              > 
+                <Typography
+                  variant={'h4'}
+                  color={'#333'}
+                  textAlign={'center'}
+                >
+                  {name.charAt(0).toUpperCase() + name.slice(1)}
+                </Typography>
+
+                <Typography
+                  variant={'h4'}
+                  color={'#333'}
+                  textAlign={'center'}
+                >
+                  Quantity: {quantity}
+                </Typography>
+              
+                <Button variant="contained" onClick={() => removeItem(name)}> 
+                  Remove
+                </Button>
+              </Box>
+            ))}
           </Stack>
         </Box>
-      </Modal>
-      <Button variant="contained" onClick={handleOpen}>
-        Add New Item
-      </Button>
-      <Box border={'1px solid #333'}>
-        <Box
-          width="800px"
-          height="100px"
-          bgcolor={'#ADD8E6'}
-          display={'flex'}
-          justifyContent={'center'}
-          alignItems={'center'}
-        >
-          <Typography variant={'h2'} color={'#333'} textAlign={'center'}>
-            Inventory Items
-          </Typography>
-        </Box>
-        <Stack width="800px" height="300px" spacing={2} overflow={'auto'}>
-          {inventory.map(({name, quantity}) => (
-            <Box
-              key={name}
-              width="100%"
-              minHeight="150px"
-              display={'flex'}
-              justifyContent={'space-between'}
-              alignItems={'center'}
-              bgcolor={'#f0f0f0'}
-              paddingX={5}
-            >
-              <Typography variant={'h3'} color={'#333'} textAlign={'center'}>
-                {name.charAt(0).toUpperCase() + name.slice(1)}
-              </Typography>
-              <Typography variant={'h3'} color={'#333'} textAlign={'center'}>
-                Quantity: {quantity}
-              </Typography>
-              <Button variant="contained" onClick={() => removeItem(name)}>
-                Remove
-              </Button>
-            </Box>
-          ))}
-        </Stack>
       </Box>
     </Box>
+    
   )
 }
